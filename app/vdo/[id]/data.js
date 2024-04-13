9da6f914555cb17050a31c123e378eb68aef9da4
เอ็ก <136637894+natthawutphet@users.noreply.github.com>
2024-04-13 17:26:43 +0700

    Delete app/vdo directory

--- app/vdo/[id]/data.js
+++
@@ -1,9 +0,0 @@
-export async function getData(id) {
- 
-    const res = await fetch(`https://api.service-ads.com/getapi`);
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    return res.json();
-  }
-
